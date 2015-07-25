5999d2323a02f61ca8aa6d92712e819e07d9adf9
aliang <aliang2344@gmail.com>
2015-07-25 13:40:10 -0400

    adding mongo conn

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://blog.theworldj.tk:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("connected to mongo :) !");
+  }
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
